2565f74ac52919f1a8919f00739c58bcbd34c2db
Fenner Macrae <13209544+fennerm@users.noreply.github.com>
2018-02-03 16:38:16 -0800

    Added parser tests

--- test/test_parser.py
+++ test/test_parser.py
@@ -1,7 +1,4 @@
-from pytest import (
-    fixture,
-    mark,
-)
+from pytest import fixture
 
 from i3ark.parser import *
 
@@ -14,34 +11,77 @@ def vsplit_parser():
 
 
 @fixture
+def hsplit_parser():
+    parser = YamlParser("test/test_cases/valid/hsplit_container.yml")
+    parser.parse()
+    return parser
+
+
+@fixture
+def tabbed_parser():
+    parser = YamlParser("test/test_cases/valid/tabbed_container.yml")
+    parser.parse()
+    return parser
+
+
+@fixture
+def stacked_parser():
+    parser = YamlParser("test/test_cases/valid/stacked_container.yml")
+    parser.parse()
+    return parser
+
+
+@fixture
 def term():
     return "i3-sensible-terminal -e \"i3-sensible-terminal\" &"
 
 
-class TestParsing():
-    @fixture
-    def vsplit(self):
-        return "i3-msg split v"
+@fixture
+def parent():
+    return "i3-msg focus parent"
 
-    @fixture
-    def hsplit(self):
-        return "i3-msg split h"
 
-    @fixture
-    def stack(self):
-        return "i3-msg layout stacking"
+@fixture
+def subterms(term, parent):
+    return [term, parent, term, parent]
 
-    @fixture
-    def tab(self):
-        return "i3-msg layout tabbed"
 
-    @fixture
-    def parent(self):
-        return "i3-msg focus parent"
+@fixture
+def vsplit(subterms):
+    return ["i3-msg split v"] + subterms
 
-    def test_vsplit_parsed_correctly(self, vsplit, hsplit, parent, stack,
-                                     tab, vsplit_parser, term):
-        assert vsplit_parser.instructions == [
-            vsplit, term, parent, term, parent, hsplit, term, parent, term,
-            parent, stack, term, parent, term, parent, tab, term, parent, term,
-            parent]
+
+@fixture
+def hsplit(subterms):
+    return ["i3-msg split h"] + subterms
+
+
+@fixture
+def stack(subterms):
+    return ["i3-msg layout stacking"] + subterms
+
+
+@fixture
+def tab(subterms):
+    return ["i3-msg layout tabbed"] + subterms
+
+
+@fixture
+def shared_instructions(vsplit, stack, tab, hsplit):
+    return vsplit + hsplit + stack + tab
+
+
+def test_vsplit_parsed_correctly(vsplit, shared_instructions, vsplit_parser):
+    assert vsplit_parser.instructions == vsplit + shared_instructions
+
+
+def test_hsplit_parsed_correctly(hsplit, shared_instructions, hsplit_parser):
+    assert hsplit_parser.instructions == hsplit + shared_instructions
+
+
+def test_tabbed_parsed_correctly(tab, shared_instructions, tabbed_parser):
+    assert tabbed_parser.instructions == tab + shared_instructions
+
+
+def test_stacked_parsed_correctly(stack, shared_instructions, stacked_parser):
+    assert stacked_parser.instructions == stack + shared_instructions
